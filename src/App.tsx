53feec10f113b5d559491ee5fb79508f4cb58a6c
joyceiphone <joyceliu@joyces-Air.fios-router.home>
2020-11-11 11:15:31 -0500

    primitive version done

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,8 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import 'react-toastify/dist/ReactToastify.css';
 import Header from './components/Header';
 import Wrapper from './components/Wrapper';
-import NowWhat from './components/NowWhat';
+
+import DashBoard from './components/DashBoard';
 
 const store = createStore();
 const theme = createMuiTheme({
@@ -30,7 +31,7 @@ const App = () => (
     <Provider store={store}>
       <Wrapper>
         <Header />
-        <NowWhat />
+        <DashBoard />
         <ToastContainer />
       </Wrapper>
     </Provider>
